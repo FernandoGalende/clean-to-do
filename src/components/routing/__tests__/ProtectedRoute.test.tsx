80b01bd7cadeea0a0f8da7596a49f8bfff3fb02f
fer.galende <fer.galende@aircall.io>
2024-01-21 20:07:44 +0100

    test(routes): add test suit to ProtectedRoute

---
+++ src/components/routing/__tests__/ProtectedRoute.test.tsx
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/react';
+import { test, vi } from 'vitest';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { ProtectedRoute } from '../ProtectedRoute';
+
+import * as useAuthContext from 'src/context/AuthContext/useAuth';
+
+describe('ProtectedRoute', () => {
+  test('Should renders children when authenticated', () => {
+    vi.spyOn(useAuthContext, 'useAuth').mockImplementation(() => ({
+      login: () => {},
+      logout: () => {},
+      isAuthenticated: true,
+      loading: false,
+      error: false
+    }));
+    render(
+      <Router>
+        <ProtectedRoute>
+          <div data-testid="protected-content">Protected Content</div>
+        </ProtectedRoute>
+      </Router>
+    );
+
+    const protectedContent = screen.getByTestId('protected-content');
+    expect(protectedContent).toBeInTheDocument();
+  });
+
+  test('Should not renders children when authenticated', () => {
+    vi.spyOn(useAuthContext, 'useAuth').mockImplementation(() => ({
+      login: () => {},
+      logout: () => {},
+      isAuthenticated: false,
+      loading: false,
+      error: false
+    }));
+    render(
+      <Router>
+        <ProtectedRoute>
+          <div data-testid="protected-content">Protected Content</div>
+        </ProtectedRoute>
+      </Router>
+    );
+
+    const protectedContent = screen.queryByTestId('protected-content');
+    expect(protectedContent).not.toBeInTheDocument();
+  });
+});
